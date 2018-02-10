ef7fb05621661ed0c703d549576926799fd4c7eb
Akhi5c8 <36219004+Akhi5c8@users.noreply.github.com>
2018-02-10 15:10:11 +0530

    Saved files

---
+++ server.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const path=require('path');
+const bodyParser = require('body-parser');
+const mysql      = require('mysql');
+const mysqlConfig = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'newsfeed'
+  };  
+  const app=express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+if(process.env.NODE_ENV!=="production"){
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+}
+app.post("/api/user/login",(request,response)=>{
+    const body=request.body;
+    const connection=mysql.createConnection(mysqlConfig);
+    connection.connect();
+    connection.query('SELECT id,name,email FROM user WHERE email=? and password=?',[body.email,body.password],function (error, results, fields) {
+        if (error) {
+            response.status(500).json({"error":error});
+                }
+                else
+                {
+                    const user=results[0];
+                    if(user)
+                    {
+                        response.status(200).json(user);
+                    }
+                   else{
+                       response.status(400).json({"error":"user does not exist"});
+                   } 
+                }
+                        console.log('The solution is: ', results[0]);
+                        connection.end();
+      });
+});
+if(process.env.NODE_ENV==="production"){
+    app.use(expres.static(path.join(__dirname,'dist')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(_dirname,'dist','index.html'));
+    });
+}
+app.listen(3000,()=>console.log('Example app listening on port 3000!'));
--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -1,10 +1,28 @@
+import { error } from "util";
 export default class LoginController {
-    constructor() {
+    constructor($http) {
+        this.$http=$http;
         this.email="";
         this.password="";
+        this.error="";
     }
    doLogin(){
-        alert("login button works"); 
+       var body={"email":this.email,
+                  "password":this.password
+     };
+     this.$http.post("http://localhost:3000/api/user/login",body) 
+     .then((success)=>{
+         //server responded with success
+alert("success");
+        },(error)=>{ 
+         //server responded with error
+         this.error=error.data.error;
+     })
+    .catch((exception)=>{
+        //Network exception
+console.error(exception);
+    })
     }
 
 }
+LoginController.$inject=["$http"];
